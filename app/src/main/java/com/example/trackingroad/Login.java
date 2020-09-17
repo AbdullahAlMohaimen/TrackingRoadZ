db2796aa139c74b60283b9172a5cff2e40168bd6
Abdullah AL Mohaimen <49594874+AbdullahAlMohaimenHasan@users.noreply.github.com>
2020-09-17 23:20:18 +0600

    add speed

--- app/src/main/java/com/example/trackingroad/Login.java
+++ app/src/main/java/com/example/trackingroad/Login.java
@@ -1,15 +1,29 @@
 package com.example.trackingroad;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.location.LocationManager;
+import android.media.audiofx.BassBoost;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -22,11 +36,90 @@ import com.google.firebase.auth.FirebaseAuth;
 public class Login extends AppCompatActivity {
 
     TextView setProfileNameText;
-    Button staticButton,mapButton,vehicleButton,locationButton,gasStationButton,exitButton,internetConnection,distance;
+    static TextView distance,time;
+    Button staticButton,mapButton,vehicleButton,locationButton,gasStationButton,exitButton,
+            internetConnection,distanceTwoPlace,btnStart,btnStop,btnPause;
     ImageButton profileSetting,logout,vehicleInformation;
 
+    static boolean status;
+    LocationManager locationManager;
+    static long startTime,endTime;
+    static ProgressDialog progressDialog;
+    static int p=0;
+
+    LocationService myService;
     FirebaseAuth mAuth;
 
+    private ServiceConnection sc=new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+
+            LocationService.LocalBinder binder=(LocationService.LocalBinder)iBinder;
+            myService=binder.getServices();
+            status=true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+            status=false;
+        }
+    };
+
+
+    @Override
+    protected void onDestroy() {
+        if(status==true)
+        {
+            unbindService();
+        }
+        super.onDestroy();
+    }
+
+    private void unbindService() {
+
+        if(status==false)
+        {
+            return;
+        }
+        Intent i=new Intent(getApplicationContext(),LocationService.class);
+        unbindService(sc);
+        status=false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(status==false)
+        {
+            super.onBackPressed();
+        }
+        else
+        {
+            moveTaskToBack(true);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode)
+        {
+            case 1000:
+            {
+                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+                {
+                    Toast.makeText(this,"GRANTED",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(this,"DENIED",Toast.LENGTH_SHORT).show();
+                }
+            }
+            return;
+        }
+    }
+
+    //ON CREATE//
+    ///////////////
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -34,6 +127,25 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         this.setTitle("Home");
+
+        //Request Permission
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED )
+        {
+            requestPermissions(new String[]{
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            },1000);
+        }
+
+        distance=(TextView)findViewById(R.id.distance);
+        time=(TextView)findViewById(R.id.time);
+        btnStart=(Button)findViewById(R.id.btnStart);
+        btnPause=(Button)findViewById(R.id.btnPause);
+        btnStop=(Button)findViewById(R.id.btnStop);
+
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
@@ -46,7 +158,7 @@ public class Login extends AppCompatActivity {
         locationButton=(Button)findViewById(R.id.locationId);
         gasStationButton=(Button)findViewById(R.id.gasStationId);
         exitButton=(Button)findViewById(R.id.exitId);
-        distance=(Button)findViewById(R.id.distanceId);
+        distanceTwoPlace=(Button)findViewById(R.id.distanceTwoPlaceId);
         internetConnection=(Button)findViewById(R.id.checkInternetId);
 
         mAuth=FirebaseAuth.getInstance();
@@ -109,9 +221,6 @@ public class Login extends AppCompatActivity {
             }
         });
 
-
-
-
         exitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -169,7 +278,7 @@ public class Login extends AppCompatActivity {
         });
 
 
-        distance.setOnClickListener(new View.OnClickListener() {
+        distanceTwoPlace.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -179,7 +288,120 @@ public class Login extends AppCompatActivity {
             }
         });
 
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkGPS();
+                locationManager=(LocationManager)getSystemService(LOCATION_SERVICE);
+
+                if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+                    return;
+
+                if(status==false)
+                {
+                    bindService();
+                }
+                progressDialog=new ProgressDialog(Login.this);
+                progressDialog.setIndeterminate(true);
+                progressDialog.setCancelable(false);
+                progressDialog.setMessage("Getting Location....");
+                progressDialog.show();
+
+
+                btnStart.setVisibility(View.GONE);
+                btnPause.setVisibility(View.VISIBLE);
+                btnPause.setText("Pause");
+                btnStop.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(btnPause.getText().toString().equalsIgnoreCase("Pause"))
+                {
+                    btnPause.setText("Resume");
+                    p=1;
+
+                }
+
+                else if(btnPause.getText().toString().equalsIgnoreCase("Resume"))
+                {
+                    checkGPS();
+                    locationManager=(LocationManager)getSystemService(LOCATION_SERVICE);
+
+                    if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+                        return;
+
+                    btnPause.setText("Pause");
+                    p=0;
+                }
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(status==true)
+                {
+                    unbindService();
+                }
+                btnStart.setVisibility(View.VISIBLE);
+                btnPause.setText("Pause");
+                btnPause.setVisibility(View.GONE);
+                btnStop.setVisibility(View.GONE);
+            }
+        });
     }
+
+    private void checkGPS() {
+
+        locationManager=(LocationManager)getSystemService(LOCATION_SERVICE);
+
+        if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+            showGPSDisabledAlert();
+    }
+
+    private void showGPSDisabledAlert() {
+
+        AlertDialog.Builder alertDialogBuilder=new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("Enable GPS to use application")
+                .setCancelable(false)
+                .setPositiveButton("Enable GPS", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        Intent intent=new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(intent);
+
+                    }
+                });
+        alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                dialogInterface.cancel();
+            }
+        });
+        AlertDialog alert=alertDialogBuilder.create();
+        alert.show();
+    }
+
+
+    private void bindService() {
+
+        if(status==true)
+            return;
+
+        Intent i=new Intent(getApplicationContext(),LocationService.class);
+        bindService(i,sc,BIND_AUTO_CREATE);
+        status=true;
+        startTime=System.currentTimeMillis();
+
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
